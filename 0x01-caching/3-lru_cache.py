c9fd02245d4380ae6530c5908774ef60204bab9c
SamZec <samzec4@gmail.com>
2023-10-26 08:36:04 +0000

    Corrections

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -2,6 +2,9 @@
 """ 3-lru_cache.py - module for LRUCache class """
 
 
+from typing import OrderedDict
+
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -9,23 +12,23 @@ class LRUCache(BaseCaching):
     """ inherits from BaseCaching for caching system """
     def __init__(self):
         """ Initialize caching objects """
+        self.order = OrderedDict()
         super().__init__()
 
     def put(self, key, item):
         """ Add an item in the cace """
-        if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                if key in self.cache_data.keys():
-                    del self.cache_data[key]
-                    self.cache_data[key] = item
-                else:
-                    dkey = list(self.cache_data.keys())[0]
-                    del self.cache_data[dkey]
-                    print(f"DISCARD: {dkey}")
-                    self.cache_data[key] = item
-            else:
-                self.cache_data[key] = item
+        if key and item:
+            self.order[key] = item
+            self.order.move_to_end(key)
+            self.cache_data[key] = item
+        if len(self.cache_data) > self.MAX_ITEMS:
+            dkey = list(self.order.keys())[0]
+            del self.cache_data[dkey]
+            print(f"DISCARD: {dkey}")
+            self.order.popitem(last=False)
 
     def get(self, key):
         """ Get an item from cache by key """
+        if key in self.cache_data:
+            self.order.move_to_end(key)
         return self.cache_data.get(key)
