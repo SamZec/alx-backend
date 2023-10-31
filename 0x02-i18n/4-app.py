1fb5f684303ebfb31aac07b3b992d1682b6b3b26
SamZec <samzec4@gmail.com>
2023-10-31 17:21:20 +0000

    4. Force locale with URL parameter

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""4-app.py - Force locale with URL parameter """
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Locale configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """determine the best match supported languages."""
+    locale = request.args.get('locale')
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """simple route"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
