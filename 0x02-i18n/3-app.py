4fa095c23078a4076c272540bbe1c438da85d059
SamZec <samzec4@gmail.com>
2023-10-31 12:09:12 +0000

    3. Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""1-app.py - Basic Babel setup """
+"""3-app.py - Parametrize templates """
 
 
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, gettext
 
 
 app = Flask(__name__)
@@ -13,13 +13,13 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale() -> str:
     """determine the best match supported languages."""
-    return request.accept_lanquages.best_match(["en", "fr"])
+    return request.accept_languages.best_match(["en", "fr"])
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """simple route"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
