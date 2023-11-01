23c3a2710086bcb135d48ccaa694481dd2a30c34
SamZec <samzec4@gmail.com>
2023-11-01 06:38:09 +0000

    Correct error

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -23,7 +23,7 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale() -> str:
     """determine the best match supported languages."""
-    return request.accept_lanquages.best_match(app.config["LANGUAGES"])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/', strict_slashes=False)
