838e5674a273d60a0c46b67583e8e9874939b0d6
SamZec <samzec4@gmail.com>
2023-12-01 12:21:00 +0000

    10. Writing the job creation function

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,23 @@
+module.exports = function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  jobs.forEach((item) => {
+    const job = queue.create('push_notification_code_3', item);
+      job
+        .on('enqueue', () => {
+          console.log('Notification job created:', job.id);
+	})
+        .on('complete', () => {
+          console.log('Notification job completed');
+	})
+        .on('failed', (err) => {
+          console.log(`Notification job ${job.id} failed: ${err}`);
+	})
+        .on('progress', (progress, data) => {
+          console.log(`Notification job #${job.id} ${progress}% complete`);
+	});
+      job.save();
+  });
+};
